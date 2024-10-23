0ecb12771667f3ad56ed6cd55a1bf6d64a2e2575
@ethsol <mmx233@gmail.com>
2024-10-23 21:32:20 +0200

    tap 1 hold F1 added

--- keyboards/kinesis/keymaps/macsek/keymap.c
+++ keyboards/kinesis/keymaps/macsek/keymap.c
@@ -32,16 +32,71 @@
 *                                 `--------------------'         `--------------------'
 */
 
+
+
+// void dance_1_finished(tap_dance_state_t *state, void *user_data) {
+//     if (state->count == 1) {
+//         tap_code(KC_1);  // Tap-ra az 1-et küldi
+//     } else {
+//         tap_code(KC_F1); // Dupla tap-ra az F1-et küldi
+//     }
+// }
+
+// void dance_1_reset(tap_dance_state_t *state, void *user_data) {
+//     if (state->pressed) {
+//         tap_code(KC_F1);  // Hold-ra az F1-et küldi
+//     }
+// }
+
+void dance_1_finished(tap_dance_state_t *state, void *user_data) {
+    if (state->pressed) {
+        tap_code(KC_F1);  // Hold-ra az F1-et küldi
+    } else if (state->count == 1) {
+        tap_code(KC_1);   // Tap-ra az 1-et küldi
+    }
+}
+
+void dance_1_reset(tap_dance_state_t *state, void *user_data) {
+    // Nincs szükség külön reset logikára ebben az esetben
+}
+
+
+
 // https://docs.qmk.fm/features/tap_dance
 // Tap Dance declarations
 enum {
     TD_CAPS_ESC,
+    TD_1_F1,
+    TD_2_F2,
+    TD_3_F3,
+    TD_4_F4,
+    TD_5_F5,
+    TD_6_F6,
+    TD_7_F7,
+    TD_8_F8,
+    TD_9_F9,
+    TD_0_F10,
+    TD_0_F11,
+    TD_M_F12,
 };
 
 // Tap Dance definitions
 tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_CAPS_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_CAPS, KC_ESC),
+    [TD_1_F1] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_1_finished, dance_1_reset),
+    //[TD_1_F1] = ACTION_TAP_DANCE_DOUBLE(KC_1, KC_F1),
+    [TD_2_F2] = ACTION_TAP_DANCE_DOUBLE(KC_2, KC_F2),
+    [TD_3_F3] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_F3),
+    [TD_4_F4] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_F4),
+    [TD_5_F5] = ACTION_TAP_DANCE_DOUBLE(KC_5, KC_F5),
+    [TD_6_F6] = ACTION_TAP_DANCE_DOUBLE(KC_6, KC_F6),
+    [TD_7_F7] = ACTION_TAP_DANCE_DOUBLE(KC_7, KC_F7),
+    [TD_8_F8] = ACTION_TAP_DANCE_DOUBLE(KC_8, KC_F8),
+    [TD_9_F9] = ACTION_TAP_DANCE_DOUBLE(KC_9, KC_F9),
+    [TD_0_F10] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_F10),
+    [TD_0_F11] = ACTION_TAP_DANCE_DOUBLE(KC_0, KC_F11),
+    [TD_M_F12] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_F12),
 };
 
 #define CAPS_ESC TD(TD_CAPS_ESC)
@@ -63,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [QWERTY] = LAYOUT(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_SCRL,  KC_PAUS,  TO(KEYPAD),XXXXXXX,
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                                                                      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+    TD_0_F10, TD(TD_1_F1),  TD_2_F2,  TD_3_F3,  TD_4_F4,  TD_5_F5,                                                                   TD_6_F6,  TD_7_F7,  TD_8_F8,  TD_9_F9,  TD_0_F11, TD_M_F12,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                                                      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSLS,
     CAPS_ESC, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                                                      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                                                      KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,
