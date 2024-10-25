36fe8adf73eee7eb76815813e78b8fa5c2920cc0
@ethsol <mmx233@gmail.com>
2024-10-25 23:48:05 +0200

    tapdance added in rules.mk

--- keyboards/kinesis/keymaps/macsek/rules.mk
+++ keyboards/kinesis/keymaps/macsek/rules.mk
@@ -8,3 +8,4 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = yes          # Audio output
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
